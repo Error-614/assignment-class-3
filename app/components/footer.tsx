20c06e9724a275f009cecd6c738c699865ead7db
Error-614 <mahlabazam614@gmail.com>
2024-11-24 01:44:39 +0500

    first commit

---
+++ app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <div>
+            <p className=" flex justify-center text-2xl font-bold text-center items-center bg-slate-600 h-14"  >footer</p>
+        </div>
+    )
+}
---
+++ app/components/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar(){
+    return(
+    <header>
+      <div className=" flex justify-center items-center text-2xl font-bold bg-slate-600 h-14  ">navbar</div>
+    </header>
+    )
+}
